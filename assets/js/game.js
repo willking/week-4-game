e2216dd05a3eb483a7fdc67ba8cf9366ffb6004a
ckingchris <christopherkingucla@gmail.com>
2017-05-11 00:12:21 -0700

    initial styling and js for star wars rpg

---
+++ assets/js/game.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+	// Character Selection
+	var $hansolo = $('#hansolo');
+	var $chewy = $('#chewy');
+	var $jabba = $('#jabba');
+	var $greedo = $('#greedo');
+
+	// Choices
+	$hansolo.on('click', function() {
+          console.log('clicked');
+    });
+	$chewy.on('click', function() {
+          console.log('clicked');
+    });
+	$jabba.on('click', function() {
+          console.log('clicked');
+    });
+	$greedo.on('click', function() {
+          console.log('clicked');
+    });
+
+});
