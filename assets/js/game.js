0ec813b14f647f9c19801661084b90c46b68a1cd
ckingchris <christopherkingucla@gmail.com>
2017-05-11 23:04:06 -0700

    fixed javascript to move single character selection and css

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,16 +8,44 @@ $(document).ready(function() {
 
 	// Choices
 	$hansolo.on('click', function() {
+		if($hansolo)
+		{
+		  $('#hansolo').appendTo('.choice')
+		  $('#chewy').appendTo('#first-enemy')
+		  $('#jabba').appendTo('#second-enemy')
+		  $('#greedo').appendTo('#third-enemy')
           console.log('clicked');
+		}
     });
 	$chewy.on('click', function() {
+		if($chewy)
+		{
+		  $('#chewy').appendTo('.choice')
+		  $('#hansolo').appendTo('#first-enemy')
+		  $('#jabba').appendTo('#second-enemy')
+		  $('#greedo').appendTo('#third-enemy')
           console.log('clicked');
+        }
     });
 	$jabba.on('click', function() {
+		if($jabba)
+		{
+		  $('#jabba').appendTo('.choice')
+		  $('#hansolo').appendTo('#first-enemy')
+		  $('#chewy').appendTo('#second-enemy')
+		  $('#greedo').appendTo('#third-enemy')
           console.log('clicked');
+        }
     });
 	$greedo.on('click', function() {
+		if($greedo)
+		{
+		  $('#greedo').appendTo('.choice')
+		  $('#hansolo').appendTo('#first-enemy')
+		  $('#chewy').appendTo('#second-enemy')
+		  $('#jabba').appendTo('#third-enemy')
           console.log('clicked');
+        }
     });
 
 });
