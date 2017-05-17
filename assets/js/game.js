a37bb6f8b254090cd1b0258284d3b9e4ba84aa05
ckingchris <christopherkingucla@gmail.com>
2017-05-17 13:42:11 -0700

    fixed appending classes in JS, cleaned up css and minimized html

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
 
-	//Audio
-	// var audioElement = $('audio');
-	// 	audioElement.attr('src', 'assets/mp3/cantina.mp3');
-	// 	audioElement.attr('autoplay', 'autoplay');
-	// 	audioElement.loop = true; 
+	// $('#gameMusic').play();
 
 	//Objects
 	var hansolo = {
@@ -63,212 +59,32 @@ $(document).ready(function() {
 		  charOptionsRow.append(charOptionCol);
 	});
 
-	$(document).on('click', '.char-img', 'char-text', function() {
-		if (hansolo) {
-	  		var charPick =$("#chosen");
-			$('.char-img').appendTo(charPick);
-			$('.char-text').appendTo(charPick);
+	$(document).on('click', '.char-option', function() {
+	
+		for (var i =0; i < choices.length; i++) {
+			$('.char-option').appendTo('.enemies');
 		}
-		if (chewy) {
-	  		var charPick =$("#chosen");
-			$('.char-img').appendTo(charPick);
-			$('.char-text').appendTo(charPick);
-		}
-		if (jabba) {
-	  		var charPick =$("#chosen");
-			$('.char-img').appendTo(charPick);
-			$('.char-text').appendTo(charPick);
-		}
-		if (greedo) {
-	  		var charPick =$("#chosen");
-			$('.char-img').appendTo(charPick);
-			$('.char-text').appendTo(charPick);
-		}
+
+		$(this).appendTo('.you').removeClass('char-option col-lg-3');
+
+		$('.char-option').removeClass('char-option col-lg-3').addClass('enemy-option col-lg-4');
+
+		$(function () {
+		    $("#character-text").remove('#character-text');
+		    $('#chosen-text').removeClass('hidden');
+		    $('#attack-button').removeClass('hidden');
+		    $('#fighting-text').removeClass('hidden');
+		    $('#enemies-text').removeClass('hidden');
+		});
 	});
 
-	// var start = true;
+	$(document).on('click', '.enemy-option', function() {
 
-	// // Character Selection
-	// var $hansolo = $('#hansolo');
-	// var $chewy = $('#chewy');
-	// var $jabba = $('#jabba');
-	// var $greedo = $('#greedo');
+		for (var i =0; i < choices.length; i++) {
+			$('.enemy-option').appendTo('.enemies').removeClass('col-lg-4').addClass('col-lg-4');
+		}
 
-	// // Choices
-	// $hansolo.on('click', function() {
-	// 	// var character = $(this).attr('data-character');
-	// 	$hansolo = {
-	// 		name: "Han Solo",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$chewy = {
-	// 		name: "Chewy",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$jabba = {
-	// 		name: "Jabba",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$greedo = {
-	// 		name: "Greedo",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	if(start)
-	// 	{
-	// 	  $('#hansolo').appendTo('.choice')
-	// 	  $('#chewy').appendTo('#first-enemy')
-	// 	  $('#jabba').appendTo('#second-enemy')
-	// 	  $('#greedo').appendTo('#third-enemy')
- //          $(function () {
- //          	$("#character-text").remove('#character-text');
- //     	  	$('#chosen-text').removeClass('hidden');
- //     	  	$('#enemies-text').removeClass('hidden');
- // 		  });
- // 		start = false;
-	// 	}
-	// 	if(start == false) {
-	// 		game();
-	// 	}
- //    });
-	// $chewy.on('click', function() {
-	// 	$hansolo = {
-	// 		name: "Han Solo",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$chewy = {
-	// 		name: "Chewy",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$jabba = {
-	// 		name: "Jabba",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$greedo = {
-	// 		name: "Greedo",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	if(start)
-	// 	{
-	// 	  $('#chewy').appendTo('.choice')
-	// 	  $('#hansolo').appendTo('#first-enemy')
-	// 	  $('#jabba').appendTo('#second-enemy')
-	// 	  $('#greedo').appendTo('#third-enemy')
- //          $(function () {
- //          	$("#character-text").remove('#character-text');
- //     	  	$('#chosen-text').removeClass('hidden');
- //     	  	$('#enemies-text').removeClass('hidden');
- // 		  });
- // 		start = false;
- //        }
- //        if(start == false) {
-	// 		game();
-	// 	}
- //    });
-	// $jabba.on('click', function() {
-	// 	$hansolo = {
-	// 		name: "Han Solo",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$chewy = {
-	// 		name: "Chewy",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$jabba = {
-	// 		name: "Jabba",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$greedo = {
-	// 		name: "Greedo",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	if(start)
-	// 	{
-	// 	  $('#jabba').appendTo('.choice')
-	// 	  $('#hansolo').appendTo('#first-enemy')
-	// 	  $('#chewy').appendTo('#second-enemy')
-	// 	  $('#greedo').appendTo('#third-enemy')
- //          $(function () {
- //          	$("#character-text").remove('#character-text');
- //     	  	$('#chosen-text').removeClass('hidden');
- //     	  	$('#enemies-text').removeClass('hidden');
- // 		  });
- // 		start = false;
- //        }
- //        if(start == false) {
-	// 		game();
-	// 	}
- //    });
-	// $greedo.on('click', function() {
-	// 	$hansolo = {
-	// 		name: "Han Solo",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$chewy = {
-	// 		name: "Chewy",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$jabba = {
-	// 		name: "Jabba",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	$greedo = {
-	// 		name: "Greedo",
-	// 		attack: 10,
-	// 		hp: 20,
-	// 		counter: 0,
-	// 	}
-	// 	if(start)
-	// 	{
-	// 	  $('#greedo').appendTo('.choice')
-	// 	  $('#hansolo').appendTo('#first-enemy')
-	// 	  $('#chewy').appendTo('#second-enemy')
-	// 	  $('#jabba').appendTo('#third-enemy')
- //          $(function () {
- //          	$("#character-text").remove('#character-text');
- //     	  	$('#chosen-text').removeClass('hidden');
- //     	  	$('#enemies-text').removeClass('hidden');
- // 		  });
- // 		start = false;
- //        }
- //        if(start == false) {
-	// 		game();
-	// 	}
- //    });
+		$(this).appendTo('.fighting').removeClass('col-lg-4');
 
-	// function game() {
-	// 	var attack = $("#attack");
-	// 	attack.on('click', function() {
-	// 		console.log('click');
-	// 	});
-	// }
+	});
 });
